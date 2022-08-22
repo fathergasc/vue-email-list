6ead9b748118284935eab10bae7c4ce1d6428083
Andrea Quaglioni <quaglioniandrea@gmail.com>
2022-08-22 13:58:16 +0200

    added basic call to api

---
+++ js/main.js
@@ -0,0 +1,12 @@
+var app = new Vue({
+    el: '#app',
+    data: {
+    },
+    methods: {
+    },
+    mounted() {
+        axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
+        .then(response => {
+            console.log(response);   
+    })}
+    });
