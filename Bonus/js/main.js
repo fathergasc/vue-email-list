d96a9392417cc4f63232219bc15dd102760c3453
Andrea Quaglioni <quaglioniandrea@gmail.com>
2022-08-22 15:01:47 +0200

    added bonus task

---
+++ Bonus/js/main.js
@@ -0,0 +1,18 @@
+var app = new Vue({
+  el: "#app",
+  data: {
+    emailsList: [],
+  },
+  methods: {},
+  mounted() {
+    this.$nextTick(function () {
+      for (let i = 0; i < 10; i++) {
+        axios
+          .get("https://flynn.boolean.careers/exercises/api/random/mail")
+          .then((response) => {
+            this.emailsList.push(response.data.response);
+          });
+      }
+    });
+  },
+});
